657932c731e04261f9c6c008a5d1b22f8a528e6e
Daya Souza <dayanalvesouza@gmail.com>
2024-01-08 18:23:48 -0300

    Criado pagina index da view post para visualizar produtos cadastrados

---
+++ app/Views/posts/index.php
@@ -0,0 +1,15 @@
+<div class="container py-5">
+    <div class="card">
+        <div class="card-header bg-info text-white">
+            PRODUTOS 
+            <div class="float-right">
+                <a href="<?= URL ?>/posts/cadastrar" class="btn btn-light">Cadastrar Novo</a>
+            </div>
+        </div>
+        <div class="card-body">
+            <p>
+                Lista de produtos Cadastrados
+            </p>
+        </div>
+    </div>
+</div>
