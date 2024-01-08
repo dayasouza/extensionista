1270916d57e2b2dc10978caad063e3d5e1e543d8
Daya Souza <dayanalvesouza@gmail.com>
2024-01-08 18:02:58 -0300

    Criado classe Database e as funções para inserir,  consultar dadoos no banco

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -7,7 +7,11 @@ class Database{
     private $password = '';
     private $db = 'precificacao';
     private $porta = '3306' ;
-    private $dbh ;    
+
+    private $dbh ;  
+    private $stmt ; 
+    
+    
 
     public function __construct()
     {
@@ -23,5 +27,72 @@ class Database{
             die();
         }
     }
+
+    public function query($sql){
+        // Prepara a consulta SQL usando a conexão do banco de dados
+        $this -> stmt = $this -> dbh -> prepare($sql);
+    }
+
+    public function bind($paremtro, $valor, $tipo = null)
+    {
+        // Verifica se o tipo do parâmetro não foi fornecido
+        if (is_null($tipo)) {
+            // Detecta automaticamente o tipo do parâmetro com base no valor fornecido
+            switch (true) {
+                case is_int($valor):
+                    $tipo = PDO::PARAM_INT;
+                    break;
+                case is_bool($valor):
+                    $tipo = PDO::PARAM_BOOL;
+                    break;
+                case is_null($valor):
+                    $tipo = PDO::PARAM_NULL;
+                    break;
+                
+                default:
+                    $tipo = PDO::PARAM_STR;
+                    break;
+            }
+        }
+        // Associa o valor ao parâmetro na consulta SQL preparada
+        $this -> stmt -> bindValue($paremtro, $valor, $tipo);
+    }
+
+    public function executa()
+    {
+        // Executa a consulta SQL preparada e retorna o resultado da execução
+        return $this -> stmt -> execute();
+    }
+
+    public function resultado()
+    {
+        // Executa a consulta SQL preparada
+        $this -> executa();
+        // Retorna a primeira linha do resultado como um objeto
+        return $this -> stmt -> fetch (PDO::FETCH_OBJ);
+    }
+
+    public function resultados()
+    {
+        // Executa a consulta SQL preparada
+        $this -> executa();
+        // Retorna todas as linhas do resultado como um array de objetos
+        return $this -> stmt -> fetchAll (PDO::FETCH_OBJ);
+    }
+
+    public function totalResultados()
+    {   
+        // Utiliza o método rowCount() do objeto $stmt para obter o número total de linhas afetadas
+        return $this-> stmt -> rowCount();
+    }
+
+    public function ultimoIdInserido()
+    {
+        // Utiliza o método lastInsertId() do objeto $dbh para obter o último ID inserido
+        return $this-> dbh -> lastInsertId();
+    }
+
+
+
 
 }
